zen4s/llmc.copy

slurm for multinode mpirun for single node

train_gpt2.cu
@@ -347,16 +347,13 @@ __device__ __forceinline__ void stochastic_rounding(float in, float *out, unsign
 
 // Parameters specific to training on multiple GPUs.
 typedef struct {
+    bool slurm_managed;
     int process_rank;      // Rank of this process among all MPI processes. 0 if no multi-GPU.
     int num_processes;     // Total number of processes. 1 if no multi-GPU.
     int local_device_idx;  // This process GPU index on current machine. 0 if no multi-GPU.
 
     // Zero Redundancy Optimizer stage - https://fairscale.readthedocs.io/en/stable/deep_dive/oss_sdp_fsdp.html
-    // 0-Disabled
-    // 1-Optimizer State Sharding (OSS)
-    // 2-Optimizer + Gradient State Sharding (SDP)
-    // 3-Optimizer + Gradient + Horizontal Model Sharding (FSDP)
-    int zero_stage;
+    int zero_stage;        // 0-Disabled, 1-OSS, 2-SDP, 3-FSDP
     size_t shard_num_parameters;
     size_t shard_offset;
 #ifdef MULTI_GPU
@@ -367,61 +364,52 @@ typedef struct {
 // one global variable to hold the multi-GPU configuration for this process
 MultiGpuConfig multi_gpu_config;
 
-#ifdef MULTI_GPU
-// Determine which GPU this process should use.
-// Processes on the same machines use different GPU indicies. Processes on other machines don't.
-// Copied from NCCL examples: https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/examples.html#example-2-one-device-per-process-or-thread
-int multi_gpu_get_local_device_idx(int process_rank, int num_processes) {
-  char hostname[1024];
-  hostname[1023] = '\0';
-  // All processes on the same machine will share the same hostname.
-  gethostname(hostname, 1023);
-  for (int i=0; i < 1024; i++) {
-    if (hostname[i] == '.') {
-        hostname[i] = '\0';
-        break;
-    }
-  }
-  uint64_t hostname_hash = 5381;
-  for (int c = 0; hostname[c] != '\0'; c++){ hostname_hash = ((hostname_hash << 5) + hostname_hash) ^ hostname[c]; }
-
-  // Distribute all hostname hashes to all processes.
-  uint64_t* all_hostsname_hashes = (uint64_t*)malloc(num_processes * sizeof(uint64_t));
-  all_hostsname_hashes[process_rank] = hostname_hash;
-  mpiCheck(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, all_hostsname_hashes, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
-
-  // Identify which GPU we need to use.
-  int local_device_idx = 0;
-  for (int current_process = 0; current_process < num_processes; ++current_process) {
-     if (current_process == process_rank) {
-      // Found my gpu, local_device_idx now has my target GPU index.
-      break;
-     }
-     if (all_hostsname_hashes[current_process] == all_hostsname_hashes[process_rank]) {
-      // This process ID runs on the same machine, but it's not me, skip this GPU
-      local_device_idx++;
-     }
-  }
-
-  free(all_hostsname_hashes);
-  return local_device_idx;
-}
-#endif
-
 MultiGpuConfig multi_gpu_config_init(int *argc, char ***argv) {
 #ifdef MULTI_GPU
-    // Initialize MPI.
     MultiGpuConfig result;
-    mpiCheck(MPI_Init(argc, argv));
-    mpiCheck(MPI_Comm_rank(MPI_COMM_WORLD, &result.process_rank));
-    mpiCheck(MPI_Comm_size(MPI_COMM_WORLD, &result.num_processes));
-    result.local_device_idx = multi_gpu_get_local_device_idx(result.process_rank, result.num_processes);
-    cudaCheck(cudaSetDevice(result.local_device_idx));
     ncclUniqueId nccl_id;
-    if (result.process_rank == 0) {
-        ncclCheck(ncclGetUniqueId(&nccl_id));
+
+    char *slurm_job_id = getenv("SLURM_JOB_ID");
+    if (slurm_job_id != NULL) {
+        result.slurm_managed = true;
+        result.process_rank = atoi(getenv("SLURM_PROCID"));
+        result.num_processes = atoi(getenv("SLURM_NTASKS"));
+        result.local_device_idx = atoi(getenv("SLURM_LOCALID"));
+
+        char *dfs_path = getenv("DFS_PATH");
+        assert(dfs_path != NULL);
+        FILE* idFile;
+        static char filename[256];
+        snprintf(filename, sizeof(filename), "%s/ncclUniqueId_%s.dat", dfs_path, slurm_job_id);
+
+        if (result.process_rank == 0) { // Generate the NCCL unique ID at rank 0 and write it to a file
+            ncclCheck(ncclGetUniqueId(&nccl_id));
+            idFile = fopen(filename, "wb");
+            assert(idFile != NULL);
+            fwrite(&nccl_id, sizeof(nccl_id), 1, idFile);
+            fclose(idFile);            
+        } else {                        // Other ranks wait until the file is available and read the unique ID
+            do {
+                idFile = fopen(filename, "rb");
+                if (idFile != NULL) break;
+                usleep(100000);
+            } while (idFile == NULL);
+            fread(&nccl_id, sizeof(nccl_id), 1, idFile);
+            fclose(idFile);
+        }
+        printf("Started Multi Node training managed by Slurm: " );
+    } else {
+        result.slurm_managed = false;
+        mpiCheck(MPI_Init(argc, argv));
+        mpiCheck(MPI_Comm_rank(MPI_COMM_WORLD, &result.process_rank));
+        mpiCheck(MPI_Comm_size(MPI_COMM_WORLD, &result.num_processes));
+        result.local_device_idx = result.process_rank;
+        if (result.process_rank == 0) ncclCheck(ncclGetUniqueId(&nccl_id));
+        mpiCheck(MPI_Bcast((void *)&nccl_id, sizeof(nccl_id), MPI_BYTE, 0, MPI_COMM_WORLD));
+        printf("Started Single Node training managed by MPI: " );
     }
-    mpiCheck(MPI_Bcast((void *)&nccl_id, sizeof(nccl_id), MPI_BYTE, 0, MPI_COMM_WORLD));
+    printf("ProcessID:%d, NumProcess::%d, DeviceId:%d\n", result.process_rank, result.num_processes, result.local_device_idx);
+    cudaCheck(cudaSetDevice(result.local_device_idx));
     ncclCheck(ncclCommInitRank(&result.nccl_comm, result.num_processes, nccl_id, result.process_rank));
     return result;
 #else
@@ -438,7 +426,9 @@ MultiGpuConfig multi_gpu_config_init(int *argc, char ***argv) {
 void multi_gpu_config_free(const MultiGpuConfig* multi_gpu_config) {
 #ifdef MULTI_GPU
     ncclCheck(ncclCommDestroy(multi_gpu_config->nccl_comm));
-    mpiCheck(MPI_Finalize());
+    if (!multi_gpu_config->slurm_managed) {
+        mpiCheck(MPI_Finalize());
+    }
 #endif
 }
 
@@ -1904,6 +1894,7 @@ typedef struct {
     int* targets; // the target tokens for the current forward pass
     float mean_loss; // after a forward pass with targets, will be populated with the mean loss
     float accumulated_mean_loss; // Mean loss after aggregating it on all GPUs
+    float* loss_buffer; // GPU buffer to avg loss across process
     floatX* cpu_losses; // CPU buffer to copy the losses to, allocated with cudaMallocHost
     unsigned long long rng_state; // the RNG state for seeding stochastic rounding etc.
     int use_master_weights;
@@ -1983,6 +1974,7 @@ void gpt2_build_from_checkpoint(GPT2 *model, const char* checkpoint_path) {
     model->inputs = NULL;
     model->targets = NULL;
     model->cpu_losses = NULL;
+    model->loss_buffer = NULL;
     model->batch_size = 0;
     model->seq_len = 0;
     model->mean_loss = -1.0f; // -1.0f will designate no loss
@@ -2297,10 +2289,20 @@ void gpt2_backward(GPT2 *model) {
 }
 
 // Compute a mean of a single CPU value across all GPU processes. No-op when multi-GPU is disabled.
-float multi_gpu_cpu_float_mean(float value, const MultiGpuConfig* multi_gpu_config) {
+float multi_gpu_float_mean(float value, float *d_buffer, const MultiGpuConfig* multi_gpu_config) {
 #ifdef MULTI_GPU
-    // MPI doesn't support all reduce with mean, so we sum up, then divide.
+    if (multi_gpu_config->num_processes == 1) return value;
+
     float result;
+
+    if (multi_gpu_config->slurm_managed) {  //If the process is managed by slurm we don't need to use MPI
+        if (d_buffer == NULL) cudaCheck(cudaMalloc(&d_buffer, sizeof(float)));
+        cudaCheck(cudaMemcpy(d_buffer, &value, sizeof(float), cudaMemcpyHostToDevice));
+        ncclCheck(ncclAllReduce(d_buffer, d_buffer, sizeof(float), ncclFloat, ncclAvg, multi_gpu_config->nccl_comm, 0));
+        cudaCheck(cudaMemcpy(&result, d_buffer, sizeof(float), cudaMemcpyDeviceToHost));
+        return result;
+    }
+    // MPI doesn't support all reduce with mean, so we sum up, then divide.
     mpiCheck(MPI_Allreduce(&value, &result, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD));
     return result / multi_gpu_config->num_processes;
 #else
@@ -2315,7 +2317,7 @@ void gpt2_multi_gpu_accumulate(GPT2* model, MultiGpuConfig* multi_gpu_config) {
     NVTX_RANGE_FN();
     if (multi_gpu_config->num_processes == 1) { return; }
     // Average all losses.
-    model->accumulated_mean_loss = multi_gpu_cpu_float_mean(model->mean_loss, multi_gpu_config);
+    model->accumulated_mean_loss = multi_gpu_float_mean(model->mean_loss, model->loss_buffer, multi_gpu_config);
     if(multi_gpu_config->zero_stage == 0) {
         //  no ZERO == standard DDP: Average all gradients.
         ncclCheck(ncclAllReduce(model->grads_memory, model->grads_memory,
@@ -2749,7 +2751,7 @@ int main(int argc, char *argv[]) {
                 val_loss += model.mean_loss;
             }
             val_loss /= val_num_batches;
-            val_loss = multi_gpu_cpu_float_mean(val_loss, &multi_gpu_config);
+            val_loss = multi_gpu_float_mean(val_loss, model.loss_buffer, &multi_gpu_config);
             printf0("val loss %f\n", val_loss);
             logger_log_val(&logger, step, val_loss);
         }

deepak2002139/AI_Lead_Tesla

train_gpt2.cu
@@ -110,16 +110,13 @@ void mpi_check(int status, const char *file, int line) {
 
 // Parameters specific to training on multiple GPUs.
 typedef struct {
+    bool slurm_managed;
     int process_rank;      // Rank of this process among all MPI processes. 0 if no multi-GPU.
     int num_processes;     // Total number of processes. 1 if no multi-GPU.
     int local_device_idx;  // This process GPU index on current machine. 0 if no multi-GPU.
 
     // Zero Redundancy Optimizer stage - https://fairscale.readthedocs.io/en/stable/deep_dive/oss_sdp_fsdp.html
-    // 0-Disabled
-    // 1-Optimizer State Sharding (OSS)
-    // 2-Optimizer + Gradient State Sharding (SDP)
-    // 3-Optimizer + Gradient + Horizontal Model Sharding (FSDP)
-    int zero_stage;
+    int zero_stage;        // 0-Disabled, 1-OSS, 2-SDP, 3-FSDP
     size_t shard_num_parameters;
     size_t shard_offset;
 #ifdef MULTI_GPU
@@ -130,61 +127,52 @@ typedef struct {
 // one global variable to hold the multi-GPU configuration for this process
 MultiGpuConfig multi_gpu_config;
 
-#ifdef MULTI_GPU
-// Determine which GPU this process should use.
-// Processes on the same machines use different GPU indicies. Processes on other machines don't.
-// Copied from NCCL examples: https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/examples.html#example-2-one-device-per-process-or-thread
-int multi_gpu_get_local_device_idx(int process_rank, int num_processes) {
-  char hostname[1024];
-  hostname[1023] = '\0';
-  // All processes on the same machine will share the same hostname.
-  gethostname(hostname, 1023);
-  for (int i=0; i < 1024; i++) {
-    if (hostname[i] == '.') {
-        hostname[i] = '\0';
-        break;
-    }
-  }
-  uint64_t hostname_hash = 5381;
-  for (int c = 0; hostname[c] != '\0'; c++){ hostname_hash = ((hostname_hash << 5) + hostname_hash) ^ hostname[c]; }
-
-  // Distribute all hostname hashes to all processes.
-  uint64_t* all_hostsname_hashes = (uint64_t*)malloc(num_processes * sizeof(uint64_t));
-  all_hostsname_hashes[process_rank] = hostname_hash;
-  mpiCheck(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, all_hostsname_hashes, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
-
-  // Identify which GPU we need to use.
-  int local_device_idx = 0;
-  for (int current_process = 0; current_process < num_processes; ++current_process) {
-     if (current_process == process_rank) {
-      // Found my gpu, local_device_idx now has my target GPU index.
-      break;
-     }
-     if (all_hostsname_hashes[current_process] == all_hostsname_hashes[process_rank]) {
-      // This process ID runs on the same machine, but it's not me, skip this GPU
-      local_device_idx++;
-     }
-  }
-
-  free(all_hostsname_hashes);
-  return local_device_idx;
-}
-#endif
-
 MultiGpuConfig multi_gpu_config_init(int *argc, char ***argv) {
 #ifdef MULTI_GPU
-    // Initialize MPI.
     MultiGpuConfig result;
-    mpiCheck(MPI_Init(argc, argv));
-    mpiCheck(MPI_Comm_rank(MPI_COMM_WORLD, &result.process_rank));
-    mpiCheck(MPI_Comm_size(MPI_COMM_WORLD, &result.num_processes));
-    result.local_device_idx = multi_gpu_get_local_device_idx(result.process_rank, result.num_processes);
-    cudaCheck(cudaSetDevice(result.local_device_idx));
     ncclUniqueId nccl_id;
-    if (result.process_rank == 0) {
-        ncclCheck(ncclGetUniqueId(&nccl_id));
+
+    char *slurm_job_id = getenv("SLURM_JOB_ID");
+    if (slurm_job_id != NULL) {
+        result.slurm_managed = true;
+        result.process_rank = atoi(getenv("SLURM_PROCID"));
+        result.num_processes = atoi(getenv("SLURM_NTASKS"));
+        result.local_device_idx = atoi(getenv("SLURM_LOCALID"));
+
+        char *dfs_path = getenv("DFS_PATH");
+        assert(dfs_path != NULL);
+        FILE* idFile;
+        static char filename[256];
+        snprintf(filename, sizeof(filename), "%s/ncclUniqueId_%s.dat", dfs_path, slurm_job_id);
+
+        if (result.process_rank == 0) { // Generate the NCCL unique ID at rank 0 and write it to a file
+            ncclCheck(ncclGetUniqueId(&nccl_id));
+            idFile = fopen(filename, "wb");
+            assert(idFile != NULL);
+            fwrite(&nccl_id, sizeof(nccl_id), 1, idFile);
+            fclose(idFile);            
+        } else {                        // Other ranks wait until the file is available and read the unique ID
+            do {
+                idFile = fopen(filename, "rb");
+                if (idFile != NULL) break;
+                usleep(100000);
+            } while (idFile == NULL);
+            fread(&nccl_id, sizeof(nccl_id), 1, idFile);
+            fclose(idFile);
+        }
+        printf("Started Multi Node training managed by Slurm: " );
+    } else {
+        result.slurm_managed = false;
+        mpiCheck(MPI_Init(argc, argv));
+        mpiCheck(MPI_Comm_rank(MPI_COMM_WORLD, &result.process_rank));
+        mpiCheck(MPI_Comm_size(MPI_COMM_WORLD, &result.num_processes));
+        result.local_device_idx = result.process_rank;
+        if (result.process_rank == 0) ncclCheck(ncclGetUniqueId(&nccl_id));
+        mpiCheck(MPI_Bcast((void *)&nccl_id, sizeof(nccl_id), MPI_BYTE, 0, MPI_COMM_WORLD));
+        printf("Started Single Node training managed by MPI: " );
     }
-    mpiCheck(MPI_Bcast((void *)&nccl_id, sizeof(nccl_id), MPI_BYTE, 0, MPI_COMM_WORLD));
+    printf("ProcessID:%d, NumProcess::%d, DeviceId:%d\n", result.process_rank, result.num_processes, result.local_device_idx);
+    cudaCheck(cudaSetDevice(result.local_device_idx));
     ncclCheck(ncclCommInitRank(&result.nccl_comm, result.num_processes, nccl_id, result.process_rank));
     return result;
 #else
@@ -201,7 +189,9 @@ MultiGpuConfig multi_gpu_config_init(int *argc, char ***argv) {
 void multi_gpu_config_free(const MultiGpuConfig* multi_gpu_config) {
 #ifdef MULTI_GPU
     ncclCheck(ncclCommDestroy(multi_gpu_config->nccl_comm));
-    mpiCheck(MPI_Finalize());
+    if (!multi_gpu_config->slurm_managed) {
+        mpiCheck(MPI_Finalize());
+    }
 #endif
 }
 
@@ -504,6 +494,7 @@ typedef struct {
     int* targets; // the target tokens for the current forward pass
     float mean_loss; // after a forward pass with targets, will be populated with the mean loss
     float accumulated_mean_loss; // Mean loss after aggregating it on all GPUs
+    float* loss_buffer; // GPU buffer to avg loss across process
     floatX* cpu_losses; // CPU buffer to copy the losses to, allocated with cudaMallocHost
     float* cpu_losses_fp32; // same but fp32
     unsigned long long rng_state; // the RNG state for seeding stochastic rounding etc.
@@ -525,6 +516,7 @@ void gpt2_init_common(GPT2 *model) {
     model->targets = NULL;
     model->cpu_losses = NULL;
     model->cpu_losses_fp32 = NULL;
+    model->loss_buffer = NULL;
     // the B,T params are determined and set, fixed on first batch in forward()
     model->batch_size = 0;
     model->seq_len = 0;
@@ -1060,10 +1052,19 @@ void gpt2_backward(GPT2 *model, int* inputs) {
 }
 
 // Compute sum of a single CPU value across all GPU processes. No-op when multi-GPU is disabled.
-float multi_gpu_cpu_float_sum(float value) {
+float multi_gpu_float_sum(float value, float *d_buffer, const MultiGpuConfig* multi_gpu_config) {
 #ifdef MULTI_GPU
-    // note MPI doesn't support all reduce with mean, only sum
+    if (multi_gpu_config->num_processes == 1) return value;
+
     float result;
+    if (multi_gpu_config->slurm_managed) {  //If the process is managed by slurm we don't need to use MPI
+        if (d_buffer == NULL) cudaCheck(cudaMalloc(&d_buffer, sizeof(float)));
+        cudaCheck(cudaMemcpy(d_buffer, &value, sizeof(float), cudaMemcpyHostToDevice));
+        ncclCheck(ncclAllReduce(d_buffer, d_buffer, sizeof(float), ncclFloat, ncclSum, multi_gpu_config->nccl_comm, 0));
+        cudaCheck(cudaMemcpy(&result, d_buffer, sizeof(float), cudaMemcpyDeviceToHost));
+        return result;
+    }
+    // note MPI doesn't support all reduce with mean, only sum
     mpiCheck(MPI_Allreduce(&value, &result, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD));
     return result;
 #else
@@ -1079,8 +1080,7 @@ void gpt2_multi_gpu_loss_and_grad_reduce(GPT2* model, MultiGpuConfig* multi_gpu_
     // If there's only one process, there is nothing to do
     if (multi_gpu_config->num_processes == 1) { return; }
     // Average all losses.
-    model->accumulated_mean_loss = multi_gpu_cpu_float_sum(model->mean_loss) / multi_gpu_config->num_processes;
-    // Now average the gradients
+    model->accumulated_mean_loss = multi_gpu_float_sum(model->mean_loss, model->loss_buffer, multi_gpu_config) / multi_gpu_config->num_processes;
     if(multi_gpu_config->zero_stage == 0) {
         // no ZERO == standard DDP: Average all gradients.
         ncclCheck(ncclAllReduce(model->grads_memory, model->grads_memory,
@@ -1693,7 +1693,7 @@ int main(int argc, char *argv[]) {
                 val_loss += model.mean_loss;
             }
             val_loss /= val_num_batches;
-            val_loss = multi_gpu_cpu_float_sum(val_loss) / multi_gpu_config.num_processes;
+            val_loss = multi_gpu_cpu_float_sum(val_loss, model.loss_buffer, &multi_gpu_config) / multi_gpu_config.num_processes;
             printf0("val loss %f\n", val_loss);
             logger_log_val(&logger, step, val_loss);
         }